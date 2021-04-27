3275a68f7734610d03118d88881c0884779c61e8
dlscoccia <2054@holbertonschool.com>
2021-04-27 11:24:44 -0500

    Adding node insert function

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number) {
+if (head == NULL)
+return (NULL);
+
+listint_t *new_node = malloc(sizeof(listint_t));
+listint_t *auxiliar = *head;
+
+if (new_node == NULL)
+return (NULL);
+new_node-> n = number;
+
+if (number <= auxiliar->n){
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+while(auxiliar->next != NULL){
+if (auxiliar->next->n >= number){
+new_node->next = auxiliar->next;
+auxiliar->next = new_node;
+return (new_node);
+}
+auxiliar = auxiliar->next;
+}
+if (auxiliar->next == NULL){
+auxiliar->next = new_node;
+new_node->next = NULL;
+}
+return (new_node);
+}
