aa24a5a390ece51497abd09aa588e7d97b8002b0
dlscoccia <2054@holbertonschool.com>
2021-04-28 09:38:19 -0500

    removing first box to the array

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,13 +8,13 @@ def canUnlockAll(boxes):
     '''
     if len(boxes) == 0:
         return False
-    open = [0, ]
+    open = []
 
     for box in boxes:
         if len(box) == 0:
             break
         for key in box:
-            if key > 0 and key <= len(boxes):
+            if key > 0 and key <= len(boxes) - 1:
                 if key not in open:
                     open.append(key)
 
