9b11e2b703fdf6ea22ddf588493aca32a23a7b4e
dlscoccia <2054@holbertonschool.com>
2021-04-28 09:36:42 -0500

    adding first box to the array

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,13 +8,13 @@ def canUnlockAll(boxes):
     '''
     if len(boxes) == 0:
         return False
-    open = []
+    open = [0, ]
 
     for box in boxes:
         if len(box) == 0:
             break
         for key in box:
-            if key > 0 and key <= len(boxes) - 1:
+            if key > 0 and key <= len(boxes):
                 if key not in open:
                     open.append(key)
 
