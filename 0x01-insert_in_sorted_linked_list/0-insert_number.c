c3dfb096c197132d162e096874752133b743f6a0
dlscoccia <2054@holbertonschool.com>
2021-04-29 08:46:25 -0500

    betty styles

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,52 +3,44 @@
 #include <stdlib.h>
 
 /**
- * insert_node - Inserts a number into a sorted singly linked list.
- * @head: start of pointer
- * @number: new node
- * Return: address of the new element or NULL if it failed
+ * insert_node - insert a node into a linked list
+ * @head: pointer to list
+ * @number: data of the node
+ * Return: addrres to new node or NULL if it fails
  */
 
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *current;
+if (head == NULL)
+return (NULL);
 
-	/*Start of the list*/
-	current = *head;
+listint_t *new_node = malloc(sizeof(listint_t));
+listint_t *auxiliar = *head;
 
-	/*Allocate memory*/
-	new = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = number;
 
-	/*Validate the  new node isn't null*/
-	if (new)
-	{
-		/*store number in the structure*/
-		new->n = number;
-		new->next = NULL;
-
-		/*Search the position the number in the list*/
-		if (*head == NULL || (*head)->n > new->n)
-		{
-			/*The number is in the first position*/
-			new->next = *head;
-			*head = new;
-		}
-		else
-		{
-			/*Compare and search the postion number*/
-			while (current->next && current->next->n < new->n)
-			{
-				current = current->next;
-			}
-
-			/*Add conection of the new node with the list*/
-			new->next = current->next;
-			current->next = new;
-		}
-		return (new);
-	}
-	return (NULL);
-
+if (number <= auxiliar->n)
+{
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
+while (auxiliar->next != NULL)
+{
+if (auxiliar->next->n >= number)
+{
+new_node->next = auxiliar->next;
+auxiliar->next = new_node;
+return (new_node);
+}
+auxiliar = auxiliar->next;
+}
+if (auxiliar->next == NULL)
+{
+auxiliar->next = new_node;
+new_node->next = NULL;
+}
+return (new_node);
