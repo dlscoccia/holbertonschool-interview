19570940629f5e408401d9a7037f2b683099a91a
dlscoccia <2054@holbertonschool.com>
2021-06-26 10:37:17 -0500

    new file, logparsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,49 +1,37 @@
 #!/usr/bin/python3
+"""
+Script that reads stdin line by line and computes metrics
+"""
+
+
 import sys
-import re
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
+                '405': 0, '500': 0}
+counter = 0
+size = 0
 
+try:
+    for l in sys.stdin:
+        data = l.split()
+        data = data[::-1]
 
-parsed_lines = []
-lines = []
-number_of_lines = {}
-total_size = 0
-codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+        if len(data) > 2:
+            counter += 1
+            if counter <= 10:
+                size += int(data[0])
+                code = data[1]
 
+            if code in status_codes.keys():
+                status_codes[code] += 1
 
-def lineparser(lines):
-    for str in lines:
-        try:
-            get_split = str.split('"')
-            date_split = get_split[0].split(' - ')
-            ip = re.search("[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+", date_split[0]).group()
-            date = re.search("\[[0-9]{4}([-][0-9]{2}){2} ([0-9]{2}[:]){2}[0-9]{2}[.][0-9]+\]", date_split[1]).group()
-            get = get_split[1]
-            status_code = get_split[2].strip().split(' ')[0]
-            size = get_split[2].strip().split(' ')[1]
-            line = ''
-            if (ip and date and get in str and status_code in codes and size):
-                info = [status_code, size]
-                parsed_lines.append(info)
-                if status_code in number_of_lines:
-                    number_of_lines[status_code] += 1
-                else:
-                    number_of_lines[status_code] = 1
-        except:
-            continue
-
-for line in sys.stdin:
-    lines.append(line)
-    try:
-        if (len(lines) == 10):
-            lineparser(lines)
-            for line in parsed_lines:
-                total_size += int(line[1])
-            lines = []
-            print("File size: {}".format(total_size))
-        for code in codes:
-            try:
-                print("{}: {}".format(code, number_of_lines[code]))
-            except:
-                continue
-    except KeyboardInterrupt:
-        continue
+            if counter == 10:
+                print('File size: {}'.format(size))
+                for sc, v in sorted(status_codes.items()):
+                    if v != 0:
+                        print('{}: {}'.format(sc, v))
+                counter = 0
+finally:
+    print('File size: {}'.format(size))
+    for sc, v in sorted(status_codes.items()):
+        if v != 0:
+            print('{}: {}'.format(sc, v))
